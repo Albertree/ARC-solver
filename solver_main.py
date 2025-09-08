242891766ac8532ea788b639ddb799f094c68fcd
Kyungmin Choi <kyungmin.official0@gmail.com>
2025-09-08 21:01:08 +0900

    branch initialization

--- solver_main.py
+++ solver_main.py
@@ -3,8 +3,8 @@ from managers.arc_manager import ARCManager
 from basics.utils import printcg
 
 if __name__ == "__main__":
-    # TASK_HEX_CODE = "08ed6ac7"
-    TASK_HEX_CODE = "007bbfb7"
+    TASK_HEX_CODE = "08ed6ac7"
+    # TASK_HEX_CODE = "007bbfb7"
     task = ARCManager.from_hex_code(TASK_HEX_CODE)
 
     solver = ARCSolver(task)
